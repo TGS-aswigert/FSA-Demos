803155dcf9f663a6847a5d95746feb9524abef55
TGS_aswigert <aswigert@teksystems.com>
2022-01-17 16:54:21 -0600

    added react directory with first app demo

---
+++ React/first-app/first-react-app_end/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1>This is the header</h1>
+      <p>Here is a paragraph</p>
+      <img src='https://picsum.photos/200/300' />
+    </div>
+  )
+}
