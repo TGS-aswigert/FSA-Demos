6c809002f6dfd2827e89ec7ce8affe7c2023e3da
TGS_aswigert <aswigert@teksystems.com>
2022-01-20 09:25:36 -0600

    added refs example

---
+++ React/hooks/refs-example/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CustomTextInput from './CustomTextInput';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomTextInput />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/hooks/refs-example/src/CustomTextInput.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class CustomTextInput extends Component {
+
+  constructor() {
+    super();
+    this.textInput = React.createRef();
+    this.focusTextInput = this.focusTextInput.bind(this);
+  }
+
+  focusTextInput() {
+    this.textInput.current.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" ref={this.textInput} />
+
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.focusTextInput}
+        />
+      </div>
+    );
+  }
+};
+
