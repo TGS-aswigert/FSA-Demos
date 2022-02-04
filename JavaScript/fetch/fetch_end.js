0fb105e2ce54d5989aa885cbbe8a63558c30ca4a
TGS_aswigert <aswigert@teksystems.com>
2022-02-04 09:57:35 -0600

    added async/await example to fetch demo

--- JavaScript/fetch/fetch_end.js
+++ JavaScript/fetch/fetch_end.js
@@ -1,13 +1,31 @@
 const pokemonName = document.getElementById("name");
 const pokemonSprite = document.getElementById("sprite");
 
-function getPokemon() {
-  fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-    .then(response => response.json())
-    .then(data => {
-      pokemonName.innerHTML = data.name;
-      pokemonSprite.src = data.sprites.front_default;
-    });
+function checkStatus(response) {
+  if(response.status >= 200 && response.status <= 299) {
+    return response.json();
+  } else {
+    throw new Error(response.status + response.statusText);
+  }
 }
 
-getPokemon();
+// function getPokemon() {
+//   fetch('https://pokeapi.co/api/v2/pokemon/ditt')
+//     .then(checkStatus)
+//     .then(data => {
+//       pokemonName.innerHTML = data.name;
+//       pokemonSprite.src = data.sprites.front_default;
+//     })
+//     .catch(console.error)
+// }
+
+async function getPokemon() {
+  let response = await fetch('https://pokeapi.co/api/v2/pokemon/ditto');
+
+  let data = await checkStatus(response);
+
+  pokemonName.innerHTML = data.name;
+  pokemonSprite.src = data.sprites.front_default;
+}
+
+getPokemon().catch(console.error);
