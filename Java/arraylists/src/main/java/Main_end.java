3c769260dfb77adbd7ff9ab8d27ed632ac14a123
TGS_aswigert <aswigert@teksystems.com>
2022-02-18 13:25:27 -0600

    added ArrayList demo

---
+++ Java/arraylists/src/main/java/Main_end.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Main_end {
+    public static void main(String[] args) {
+
+        ArrayList<String> names = new ArrayList<>();
+//        System.out.println(names.size());
+
+        names.add("Abigail");
+        names.add("Michele");
+        names.add("Jonathan");
+        names.add("Abram");
+        names.add("Barry");
+        names.add("Tala");
+        names.add("Olga");
+        names.add("Mary");
+        names.add("Jon");
+        names.add("Dylan");
+
+//        System.out.println(names.size());
+//
+//        System.out.println(names.get(6));
+//
+//        names.remove(3);
+//        System.out.println(names.size());
+//
+//        names.clear();
+//        System.out.println(names.size());
+
+//        for (int i = 0; i < names.size(); i++) {
+//            System.out.println(names.get(i));
+//        }
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+    }
+}
