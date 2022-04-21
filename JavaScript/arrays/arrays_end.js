9e62e5218f04de084cc9067724f428b094f82336
TGS_aswigert <aswigert@teksystems.com>
2022-04-21 09:43:34 -0500

    added demo for JS arrays

---
+++ JavaScript/arrays/arrays_end.js
@@ -0,0 +1,12 @@
+let arr = [1, "hello", true, 8.97];
+
+arr.pop();
+
+arr.push("world");
+
+// arr.forEach(value => console.log(value));
+
+// console.log(`${arr[1]} ${arr[3]}`);
+
+console.log(arr.indexOf(true));
+console.log(arr.indexOf("Abigail"));
