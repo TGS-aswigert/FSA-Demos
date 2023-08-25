5f99f8b636107e3cda076a190fdaa7e82d9955c0
Abigail Swigert <aswigert@teksystems.com>
2023-08-25 11:43:17 -0500

    initial commit restful api demo

---
+++ Spring Boot/RestfulAPIDemo/src/test/java/com/teksystems/RestfulAPIDemo/RestfulApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teksystems.RestfulAPIDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
